fbb03ac192cacb09c0c9c7febae380d4c551168e
kchoi123 <k3vinch0i@gmail.com>
2019-04-08 09:57:24 -0700

    first push

--- schema.sql
+++ schema.sql
@@ -11,9 +11,9 @@ CREATE TABLE burger
 	PRIMARY KEY (id)
 );
 
-INSERT INTO burgers (text, devoured) VALUES ('Meeses Burger', true);
-INSERT INTO burgers (text, devoured) VALUES ('Bobbi Burger', true);
-INSERT INTO burgers (text, devoured) VALUES ('Sylvester Burger', true);
-INSERT INTO burgers (text, devoured) VALUES ('Marilyn Manson Burger', false);
-INSERT INTO burgers (text, devoured) VALUES ('Joe Biden Burger', false);
-INSERT INTO burgers (text, devoured) VALUES ('Owens Burger', false);
+INSERT INTO burger (text, devoured) VALUES ('Meeses Burger', true);
+INSERT INTO burger (text, devoured) VALUES ('Bobbi Burger', true);
+INSERT INTO burger (text, devoured) VALUES ('Sylvester Burger', true);
+INSERT INTO burger (text, devoured) VALUES ('Marilyn Manson Burger', false);
+INSERT INTO burger (text, devoured) VALUES ('Joe Biden Burger', false);
+INSERT INTO burger (text, devoured) VALUES ('Owens Burger', false);
